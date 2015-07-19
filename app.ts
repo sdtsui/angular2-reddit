511e74d834c7a19a5ac08f22570b18a8523e6ac9
sdtsui <danielt213@gmail.com>
2015-07-18 22:54:03 -0700

    app.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-app.ts
+<reference path="typings/angular2/angular2.d.ts"/>
+
+import {
+  Component,
+  View,
+  bootstrap,
+} from "angular2/angular2";
+
+@Component({
+  selector: 'hello-world'
+})
+@View({
+  template: `<div>Hello world</div>`
+})
+class HelloWorld {
+}
+bootstrap(HelloWorld);
