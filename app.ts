1a1a8912883fa5bbd589e8dc65c4d38a47594a1e
sdtsui <danielt213@gmail.com>
2015-07-18 23:26:54 -0700

    For directive not quite working

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
-<reference path="typings/angular2/angular2.d.ts"/>
+// <reference path="typings/angular2/angular2.d.ts"/>
 
 import {
   Component,
   View,
+  For,
   bootstrap,
 } from "angular2/angular2";
 
@@ -10,8 +11,19 @@ import {
   selector: 'hello-world'
 })
 @View({
-  template: `<div>Hello world</div>`
+  directives: [For],
+  template: `
+    <ul>
+      <li *for="#name of names"> Hello {{ name }}
+      </li>
+    </ul>
+  `
 })
 class HelloWorld {
+  names: Array<string>;
+
+  constructor() {
+    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+  }
 }
 bootstrap(HelloWorld);
